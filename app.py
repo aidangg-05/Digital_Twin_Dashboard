281ffa3f2babd221330373ff9eae878d2ae5b7e3
Aidan <aidang.22@ichat.sp.edu.sg>
2024-03-28 10:06:20 +0800

    added merge table code

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-
-from flask import Flask, render_template, jsonify, request, g, Response
-from filetime import to_datetime
-from math import floor
-
 import sqlite3
+
+import pandas as pd
+
+#app = Flask(__name__)
+
+# Path to SQLite database file
 database_file = r'C:\Digital_Twin\Digital_Twin_Dashboard\HistoricalGroup6.dxpdb'
 
 conn = sqlite3.connect(database_file)
-c = conn.cursor
+c = conn.cursor()
+
+conn= sqlite3.connect(database_file)
 
 def merged():
-    c.execute('CREATE TABLE table3 as SELECT * FROM HistoricalData t1 INNER JOIN NodeIdKey t2 ON t1.NodeKey = t2.NodeKey')\
-    for line in c.fetchall():
-        print(line)
+    c.execute('CREATE TABLE IF NOT EXISTS MergedData AS SELECT * FROM HistoricalData t1 INNER JOIN NodeIdKey t2 ON t1.NodeKey = t2.NodeKey')
+    conn.commit()  
+    print("Tables merged successfully!")
+    c.execute('ALTER TABLE MergedData DROP COLUMN "NodeKey:1"')  # Use double quotes to handle special characters
+    conn.commit()
+    print("Column 'NodeKey:1' dropped successfully!")
+
 merged()
 
+ # Read SQL database table into a DataFrame
+df = pd.read_sql_query("SELECT * FROM MergedData", conn)
 
+# Save DataFrame to CSV
+df.to_csv('MergedData.csv', index=False)
+
+# Close the connection
+conn.close()
